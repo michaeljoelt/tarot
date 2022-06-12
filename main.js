b6e3a03e9ab9b61abaed93b08891316f8eaf5ac9
Michael Joel <michaeljoelt@gmail.com>
2022-06-11 23:19:58 -0700

    added filter; starting working on making code easier to read

--- main.js
+++ main.js
@@ -1,97 +1,127 @@
-let simplytarot; 
-getDeck();
+let deck
+getDeck()
 
 async function getDeck() {
-            await fetch('tarot.json').then(response => response.json()).then( (data) => {simplytarot =  data}  )
+    await fetch('tarot.json').then(response => response.json()).then((data) => {
+        deck = data["cards"]
+    })
 }
-
-
-let isFront = 0
 
 document.querySelector("#draw").addEventListener('click', selectCard)
 
-function selectCard() {
-    // Returns a random integer from 0 to 77:
-    const randomNumber = Math.floor(Math.random() * 78);
-    const card = simplytarot["cards"][randomNumber]
+function selectCard(event) {
+    event.preventDefault() //prevents submit button("Draw") from refreshing page
 
-    console.log(card.name)
-    console.log(card.meaning_rev)
+    // Filter the deck
+    const userFilter = getFilter()
+    deck = deck.filter((card) => (userFilter.includes(card.type) || userFilter.includes(card.suit)) ? true : false)
 
-    displayCard(card)
+    // Returns a random integer based on deck length
+    const randomNumber = Math.floor(Math.random() * deck.length)
+    const card = deck[randomNumber]
+
+    // Check if card is drawn reversed or not (if filter allows it)
+    const isReversed = userFilter.includes("reversals") ? Math.floor(Math.random() * 2) : 0
+
+    // Prints all data to console for testing
+    console.log(
+        `Drawing random card from deck. \n(Filter: ${userFilter} | Deck size: ${deck.length}/78)
+        \nCard drawn: ${card.name} ${isReversed?"(reversed)":""}
+        \nMeaning: ${card.meaning_up}
+        \nReverse Meaning: ${card.meaning_rev}
+        \nImage Description: ${card.desc}`
+    )
+    displayCard(card, isReversed)
 }
 
-function displayCard(card) {
-    const isReversed = Math.floor(Math.random() * 2)
-    console.log(`Reversed: ${Boolean(isReversed)}`)
-
-    const divFront = document.createElement('div')
-    divFront.addEventListener('click', flipCard)
-    divFront.addEventListener('keypress', function (e) {
-        if (e.key === 'Enter') {
-            flipCard(e)
+function getFilter() {
+    let selectedCheckboxes = []
+    let checkboxes = document.querySelectorAll("input[type='checkbox']")
+    console.log("************")
+    console.log("UPDATING FILTER:")
+    console.log("************")
+    checkboxes.forEach((checkbox) => {
+        if (checkbox.checked) {
+            console.log(`${checkbox.name} ON`)
+            selectedCheckboxes.push(checkbox.name)
+        } else {
+            console.log(`${checkbox.name} OFF`)
         }
     })
-    isFront = 1
-    divFront.classList.add("front")
-    divFront.setAttribute("tabindex", "0")
-    const heading = document.createElement('h2')
-    heading.innerText = isReversed ? `${card.name} (reversed)` : card.name
-    divFront.appendChild(heading)
+    console.log("************")
+    return selectedCheckboxes
+}
 
-    const img = document.createElement('img')
-    img.src = card.image
-    if (isReversed) {
-        img.classList.add("reverse")
+function displayCard(card, isReversed) {
+
+    addCardToDom()
+
+    function addCardToDom() {
+        const fragment = document.createDocumentFragment()
+        fragment.appendChild(createFrontOfCard())
+        fragment.appendChild(createBackOfCard())
+
+        const section = document.querySelector("#drawing")
+        section.innerHTML = ""
+        section.appendChild(fragment)
+        document.querySelector("div.front").focus()
     }
-    divFront.appendChild(img)
 
-    const divBack = document.createElement('div')
-    divBack.classList.add("back");
-    divBack.classList.add("hide");
-    divBack.setAttribute("tabindex", "0")
-    divBack.addEventListener('click', flipCard)
-    divBack.addEventListener('keypress', function (e) {
-        if (e.key === 'Enter') {
-            flipCard(e)
+    function createFrontOfCard() {
+        const divFront = document.createElement('div')
+        divFront.addEventListener('click', flipCard)
+        divFront.addEventListener('keypress', function (e) {
+            if (e.key === 'Enter') {
+                flipCard(e)
+            }
+        })
+
+        divFront.classList.add("front")
+        divFront.setAttribute("tabindex", "0")
+        const heading = document.createElement('h2')
+        heading.innerText = isReversed ? `${card.name} (reversed)` : card.name
+        divFront.appendChild(heading)
+
+        const img = document.createElement('img')
+        img.src = card.image
+        if (isReversed) {
+            img.classList.add("reverse")
         }
-    })
+        divFront.appendChild(img)
+        return divFront
+    }
 
-    const paragraph1 = document.createElement('p')
-    paragraph1.innerText = `${card.name} Meaning:  \r\n${card.meaning_up}`
-    divBack.appendChild(paragraph1)
+    function createBackOfCard() {
+        const divBack = document.createElement('div')
+        divBack.classList.add("back");
+        divBack.classList.add("hide");
+        divBack.setAttribute("tabindex", "0")
+        divBack.addEventListener('click', flipCard)
+        divBack.addEventListener('keypress', function (e) {
+            if (e.key === 'Enter') {
+                flipCard(e)
+            }
+        })
+        const paragraph1 = document.createElement('p')
+        paragraph1.innerText = `${card.name} Meaning:  \r\n${card.meaning_up}`
+        divBack.appendChild(paragraph1)
 
-    const paragraph2 = document.createElement('p')
-    paragraph2.innerText = `Reverse Meaning:  \r\n${card.meaning_rev}`
-    divBack.appendChild(paragraph2)
+        const paragraph2 = document.createElement('p')
+        paragraph2.innerText = `Reverse Meaning:  \r\n${card.meaning_rev}`
+        divBack.appendChild(paragraph2)
 
-    const paragraph3 = document.createElement('p')
-    paragraph3.innerText = `Image Description: \r\n${card.desc}`
-    divBack.appendChild(paragraph3)
-
-    const fragment = document.createDocumentFragment()
-    fragment.appendChild(divFront)
-    fragment.appendChild(divBack)
-
-    const section = document.querySelector("#drawing")
-    section.innerHTML = ""
-    section.appendChild(fragment)
-
-    divFront.focus()
+        const paragraph3 = document.createElement('p')
+        paragraph3.innerText = `Image Description: \r\n${card.desc}`
+        divBack.appendChild(paragraph3)
+        return divBack
+    }
 }
 
 function flipCard(event) {
-    console.log("flipping")
-    isFront = isFront ? 0 : 1
-
+    console.log("*flipping card*")
     document.querySelector('.back').classList.toggle("hide")
     document.querySelector('.front').classList.toggle("hide")
-
-    console.log(`isFront: ${isFront}`)
     if (event.type == 'keypress') {
-        document.querySelector('div:not(.hide)').focus()
+        document.querySelector('.cards div:not(.hide)').focus()
     }
 }
-
-
-
